3e47c4bdbf2050c6ac04894e138b099c1300d8a4
Carlos Vargas <carlosvq1337@gmail.com>
2022-02-15 12:45:21 -0600

    funcionalidad de deslizar entre pantallas implementada con pantallas de prueba

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,62 @@ import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ));
+void main() => runApp(MyApp());
 
-class HomePage extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  HomePageState createState() {
-    return HomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          //Los márgenes a los lados de las páginas:
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 0),
+          child: PageViews(),
+        ),
+      ),
+    );
   }
 }
 
-class HomePageState extends State<HomePage> {
+class PageViews extends StatefulWidget {
+  @override
+  _PageViewsState createState() => _PageViewsState();
+}
+
+class _PageViewsState extends State<PageViews> {
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+    @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      children: [
+        Page1(),
+        Page2(),
+        Page3(),
+        ScanQRPage(),
+      ],
+    );
+  }
+}
+
+class ScanQRPage extends StatefulWidget {
+  @override
+  ScanQRPageState createState() {
+    return ScanQRPageState();
+  }
+}
+
+class ScanQRPageState extends State<ScanQRPage> {
   String result = "Escáner QR";
   final FlutterTts tts = FlutterTts();
 
@@ -71,6 +114,140 @@ class HomePageState extends State<HomePage> {
         onPressed: _scanQR,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+
+
+
+
+
+//COLORES
+const lightBlue = Color(0xff00bbff);
+const mediumBlue = Color(0xff00a2fc);
+const darkBlue = Color(0xff0075c9);
+
+final lightGreen = Colors.green.shade300;
+final mediumGreen = Colors.green.shade600;
+final darkGreen = Colors.green.shade900;
+
+final lightRed = Colors.red.shade300;
+final mediumRed = Colors.red.shade600;
+final darkRed = Colors.red.shade900;
+
+
+
+//Elementos de relleno (placeholders)
+class MyBox extends StatelessWidget {
+  final Color color;
+  final double height;
+  final String text;
+
+  const MyBox(this.color, {this.height, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        color: color,
+        height: (height == null) ? 150 : height,
+        child: (text == null)
+            ? null
+            : Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 50,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//  Páginas de relleno (placeholders) utilizadas para demostrar la funcionalidad
+// de deslizar entre pantallas
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: [
+            MyBox(darkGreen, height: 50),
+          ],
+        ),
+        Row(
+          children: [
+            MyBox(lightGreen),
+            MyBox(lightGreen),
+          ],
+        ),
+        MyBox(mediumGreen, text: 'Página 1'),
+        Row(
+          children: [
+            MyBox(lightGreen, height: 200),
+            MyBox(lightGreen, height: 200),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: [
+            MyBox(darkBlue, height: 50),
+            MyBox(darkBlue, height: 50),
+          ],
+        ),
+        Row(
+          children: [
+            MyBox(lightBlue),
+            MyBox(lightBlue),
+          ],
+        ),
+        MyBox(mediumBlue, text: 'Página 2'),
+        Row(
+          children: [
+            MyBox(lightBlue),
+            MyBox(lightBlue),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: [
+            MyBox(darkRed),
+            MyBox(darkRed),
+          ],
+        ),
+        MyBox(mediumRed, text: 'Página 3'),
+        Row(
+          children: [
+            MyBox(lightRed),
+            MyBox(lightRed),
+            MyBox(lightRed),
+          ],
+        ),
+      ],
     );
   }
 }
